c68115540f9caa2114f7915f62de671f56470f7e
bluartie <arteaga.jorge3@gmail.com>
2022-10-10 16:20:04 -0500

    complete player prompt function, add loop to test function

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 //playerChoice function/ prompt, lower case
 
+function playerQuestion() {
+  let promptResult = prompt("Choose between rock, paper, or scissors");
+  let playerChoice = promptResult.toLowerCase();
+}
+
+/*for (let i = 0; i < 5; i++) {
+  playerQuestion();
+}*/
+
 //computerChoice function/ random number function/num to choice function
 
 //playRound function/ else if branches for computerChoice/playerChoice

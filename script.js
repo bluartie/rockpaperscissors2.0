bc600a9d2268880329c7ba70bd7c0b5c93c626e4
bluartie <arteaga.jorge3@gmail.com>
2022-10-10 16:11:26 -0500

    write pseudocode for entire project

---
+++ script.js
@@ -0,0 +1,17 @@
+//playerChoice function/ prompt, lower case
+
+//computerChoice function/ random number function/num to choice function
+
+//playRound function/ else if branches for computerChoice/playerChoice
+
+//counter function /else if branches for playRound results
+
+//let player score = 0/ let computer score = 0
+
+/*5 round loop
+playerChoice function
+computerChoice function
+playRound function
+counter function*/
+
+//if else for final score
